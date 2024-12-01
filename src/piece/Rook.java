951ed041f682fc22205fd9c646c96db68cba527f
Aleksandra Karpova <alesksandrak@gmail.com>
2024-12-01 16:38:43 +0100

    Rook class

--- src/piece/Rook.java
+++ src/piece/Rook.java
@@ -14,4 +14,14 @@ public class Rook extends Piece{
             image = getImage("/piece/b-rook");
         }
     }
+
+    public boolean canMove(int targetColumn, int targetRow) {
+        if (isWithinBoard(targetColumn, targetRow) && !isSameSquare(targetColumn, targetRow)) {
+            // Rook can move as long as either its col or row is the same
+            if (targetColumn == preCol || targetRow == preRow) {
+                return isValidSquare(targetColumn, targetRow) && !pieceIsOnStraightLine(targetColumn, targetRow);
+            }
+        }
+        return false;
+    }
 }
