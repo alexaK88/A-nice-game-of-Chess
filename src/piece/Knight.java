1b3d3f966c8e8ec120cd0471aba1e704c8706c9a
Aleksandra Karpova <alesksandrak@gmail.com>
2024-12-02 19:59:17 +0100

    Knight class

--- src/piece/Knight.java
+++ src/piece/Knight.java
@@ -1,10 +1,12 @@
 package piece;
 
 import main.GamePanel;
+import main.Type;
 
 public class Knight extends Piece{
     public Knight(int colour, int col, int row) {
         super(colour, col, row);
+        type = Type.KNIGHT;
 
         if(colour == GamePanel.WHITE) {
             image = getImage("/piece/w-knight");
@@ -12,5 +14,17 @@ public class Knight extends Piece{
         else {
             image = getImage("/piece/b-knight");
         }
+    }
+
+    public boolean canMove(int targetColumn, int targetRow) {
+        if (isWithinBoard(targetColumn, targetRow)) {
+            // Knight can move if its; movement ration of col and row is 1:2 or 2:1
+            if (Math.abs(targetColumn - preCol) * Math.abs(targetRow - preRow) == 2) {
+                if (isValidSquare(targetColumn, targetRow)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
